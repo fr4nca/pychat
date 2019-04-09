8b6bcefb6cae91ac3fa0e460664f5723cf3abeb8
Victor Almeida França <fr4ncacc@gmail.com>
2019-04-09 12:57:15 -0300

    Multiple clients, sendToAll

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from socket import socket, SOCK_STREAM, AF_INET
 from threading import Thread
 
 HOST = '127.0.0.1'
-PORT = 23232
+PORT = 12345
 
 clientes = {}
 enderecos = {}
@@ -10,41 +10,45 @@ enderecos = {}
 server = socket(AF_INET, SOCK_STREAM)
 server.bind((HOST, PORT))
 
+
 def iniciar():
-  while True:
-    cliente, endereco = server.accept()
-    print('Conectado por ', endereco)
-    enderecos[cliente] = endereco
-    Thread(target=handle_client, args=(cliente,)).start()
+    while True:
+        cliente, endereco = server.accept()
+        print('Conectado por ', endereco)
+        enderecos[cliente] = endereco
+        Thread(target=handle_client, args=(cliente,)).start()
+
 
 def handle_client(cliente):
-  nome = cliente.recv(1024).decode("utf8")
-  bemvindoMsg = "Bem  vindo, %s. Para sair, digite {Exit}." % nome
-  cliente.send(bytes(bemvindoMsg, "utf8"))
-  entrouMsg = "%s entrou no chat.." % nome
-  sendToAll(entrouMsg)
-  clientes[cliente] = nome
-  while True:
-    msg = cliente.recv(1024).decode("utf8")
-    if msg != "{Exit}":
-      sendToAll(nome + ": " + msg)
-    else:
-      cliente.send(bytes("{Exit}", "utf8"))
-      cliente.close()
-      del clientes[cliente]
-      print("%s deixou o chat." % nome)
-      sendToAll("%s deixou o chat." % nome)
-      break
+    nome = cliente.recv(1024).decode("utf8")
+    bemvindoMsg = "Bem  vindo, %s. Para sair, digite 'Exit'." % nome
+    cliente.send(bytes(bemvindoMsg, "utf8"))
+    entrouMsg = "%s entrou no chat.." % nome
+    sendToAll(entrouMsg)
+    clientes[cliente] = nome
+    while True:
+        msg = cliente.recv(1024).decode("utf8")
+        if msg != "Exit":
+            sendToAll(nome + ": " + msg)
+        else:
+            cliente.send(bytes("Exit", "utf8"))
+            cliente.close()
+            del clientes[cliente]
+            print("%s deixou o chat." % nome)
+            sendToAll("%s deixou o chat." % nome)
+            break
+
 
 def sendToAll(msg):
-  mensagem = bytes(msg, "utf8")
-  for cliente in clientes:
-    cliente.send(mensagem)
+    mensagem = bytes(msg, "utf8")
+    for cliente in clientes:
+        cliente.send(mensagem)
+
 
 if __name__ == "__main__":
-  server.listen()
-  print("Esperando conexões")
-  ACCEPT_THREAD = Thread(target=iniciar)
-  ACCEPT_THREAD.start()
-  ACCEPT_THREAD.join()
-  server.close()
+    server.listen()
+    print("Esperando conexões")
+    ACCEPT_THREAD = Thread(target=iniciar)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    server.close()
